7f107c354c8dd0b477ce401a36964445ca4b0bda
abelgr45 <abelgr45@gmail.com>
2020-04-02 01:50:22 -0400

    update

--- js/init.js
+++ js/init.js
@@ -322,7 +322,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "Web Developer", "Photographer"],
+			strings: ["Freelancer", "Software Developer","Backend Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
